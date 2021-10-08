bbaae7444fcfe50df796d116e18029778a12e9f2
zach-detect <92181991+zach-detect@users.noreply.github.com>
2021-10-08 15:12:08 -0400

    initial upload

---
+++ ZionCamera.py
@@ -0,0 +1,318 @@
+from picamera import PiCamera, PiRenderer
+import pigpio
+import keyboard
+from operator import itemgetter
+from PIL import Image as PIL_Image
+from time import sleep
+
+
+brightness_info = (0,100,50,5)
+contrast_info = (-100,100,0,5)
+saturation_info = (-100,100,0,5)
+sharpness_info = (-100,100,0,5)
+
+iso_info = list(range(0,800+1,100))
+metering_mode = ['average', 'spot', 'backlit', 'matrix']
+exposure_info = (-25,25,0,1)
+exposure_mode_info = ['off', 'auto', 'night', 'nightpreview',
+					  'backlight', 'spotlight', 'sports', 'snow',
+					  'beach', 'verylong', 'fixedfps', 'antishake', 'fireworks']
+awb_mode_info = ['off','auto']
+awb_gains_red_info = (0.0, 8.0, 4, 0.5)
+awb_gains_blue_info = (0.0, 8.0, 4, 0.5)
+
+
+class ZionCamera(PiCamera):
+
+	def __init__(self, resolution, framerate, shutter_speed, shutter_speed_step, shutter_speed_max, gpio_ctrl=None):
+		
+		print('\nCamera Initializing...')
+		super(ZionCamera,self).__init__(resolution=resolution, framerate=framerate, sensor_mode=2)
+
+		sleep(2)
+		
+		# ~ self.framerate_range = (1, framerate)
+		#TODO fix shutter speed stuff
+		self.shutter_speed = shutter_speed
+		self.shutter_speed_default = shutter_speed
+		self.shutter_speed_step = shutter_speed_step
+		self.shutter_speed_max = shutter_speed_max
+		
+		self.BaseFilename = None
+		self.file_idx = 0
+		self.GPIO = gpio_ctrl
+		print('\nCamera Ready')
+		
+	def capture(self, filename, cropping=(0,0,1,1), useIndex=False):
+		self.zoom = cropping
+		fileToWrite = filename+'_'+str(self.file_idx)+'.jpg' if useIndex else filename+'.jpg'
+		print('\nWriting image to file '+fileToWrite)
+		if self.GPIO:
+			self.GPIO.write(self.GPIO.Camera_Trigger, True)
+		ret = super(ZionCamera,self).capture(fileToWrite)
+		if self.GPIO:
+			self.GPIO.write(self.GPIO.Camera_Trigger, False)
+		self.zoom=(0,0,1,1)
+		self.file_idx += 1
+		return ret
+		
+	def quit(self):
+		self.stop_preview()
+		self.close()
+		print('\nCamera closed')
+			
+	def interactive_preview(self, baseFilename=None, init_file_idx=0, cropping=(0,0,1,1), window=None):
+		self.file_idx = init_file_idx
+		keyboard.add_hotkey('space', self.capture, args=(baseFilename, cropping, True))
+		
+		keyboard.add_hotkey('tab', self.toggle_denoise)
+		
+		keyboard.add_hotkey('a', self.increase_brightness)
+		keyboard.add_hotkey('z', self.decrease_brightness)
+		keyboard.add_hotkey('q', self.reset_brightness)
+		keyboard.add_hotkey('s', self.increase_contrast)
+		keyboard.add_hotkey('x', self.decrease_contrast)
+		keyboard.add_hotkey('w', self.reset_contrast)
+		keyboard.add_hotkey('d', self.increase_saturation)
+		keyboard.add_hotkey('c', self.decrease_saturation)
+		keyboard.add_hotkey('e', self.reset_saturation)
+		keyboard.add_hotkey('f', self.increase_sharpness)
+		keyboard.add_hotkey('v', self.decrease_sharpness)
+		
+		keyboard.add_hotkey('`', self.set_iso, args=(iso_info[0],))
+		keyboard.add_hotkey('1', self.set_iso, args=(iso_info[1],))
+		keyboard.add_hotkey('2', self.set_iso, args=(iso_info[2],))
+		keyboard.add_hotkey('3', self.set_iso, args=(iso_info[3],))
+		keyboard.add_hotkey('4', self.set_iso, args=(iso_info[4],))
+		keyboard.add_hotkey('5', self.set_iso, args=(iso_info[5],))
+		keyboard.add_hotkey('6', self.set_iso, args=(iso_info[6],))
+		keyboard.add_hotkey('7', self.set_iso, args=(iso_info[7],))
+		keyboard.add_hotkey('8', self.set_iso, args=(iso_info[8],))
+		keyboard.add_hotkey('9', self.set_meter_mode, args=(metering_mode[0],))
+		keyboard.add_hotkey('0', self.set_meter_mode, args=(metering_mode[1],))
+		keyboard.add_hotkey('-', self.set_meter_mode, args=(metering_mode[2],))
+		keyboard.add_hotkey('=', self.set_meter_mode, args=(metering_mode[3],))
+		
+		keyboard.add_hotkey('b', self.increase_exposure)
+		keyboard.add_hotkey('g', self.decrease_exposure)
+		keyboard.add_hotkey('t', self.reset_exposure)
+		keyboard.add_hotkey('n', self.decrease_shutter_speed, args=(self.shutter_speed_step,))
+		keyboard.add_hotkey('h', self.increase_shutter_speed, args=(self.shutter_speed_step,))
+		keyboard.add_hotkey('y', self.reset_shutter_speed)
+		keyboard.add_hotkey('u', self.toggle_auto_shutter_speed)
+		
+		keyboard.add_hotkey('F1', self.set_exposure_mode, args=(exposure_mode_info[0],))
+		keyboard.add_hotkey('F2', self.set_exposure_mode, args=(exposure_mode_info[1],))
+		keyboard.add_hotkey('F3', self.set_exposure_mode, args=(exposure_mode_info[2],))
+		keyboard.add_hotkey('F4', self.set_exposure_mode, args=(exposure_mode_info[3],))
+		keyboard.add_hotkey('F5', self.set_exposure_mode, args=(exposure_mode_info[4],))
+		keyboard.add_hotkey('F6', self.set_exposure_mode, args=(exposure_mode_info[5],))
+		keyboard.add_hotkey('F7', self.set_exposure_mode, args=(exposure_mode_info[6],))
+		keyboard.add_hotkey('F8', self.set_exposure_mode, args=(exposure_mode_info[7],))
+		keyboard.add_hotkey('F9', self.set_exposure_mode, args=(exposure_mode_info[8],))
+		keyboard.add_hotkey('F10', self.set_exposure_mode, args=(exposure_mode_info[9],))
+		keyboard.add_hotkey('F11', self.set_exposure_mode, args=(exposure_mode_info[10],))
+		keyboard.add_hotkey('F12', self.set_exposure_mode, args=(exposure_mode_info[11],))
+
+		keyboard.add_hotkey('\\', self.toggle_awb)
+		keyboard.add_hotkey('apostrophe', self.increase_red_awb)
+		keyboard.add_hotkey('/', self.decrease_red_awb)
+		keyboard.add_hotkey(']', self.reset_red_awb)
+		keyboard.add_hotkey(';', self.increase_blue_awb)
+		keyboard.add_hotkey('.', self.decrease_blue_awb)
+		keyboard.add_hotkey('[', self.reset_blue_awb)
+		
+		#TODO: move preview to gui
+		if window:
+			self.start_preview(fullscreen=False, window=window)
+		else:
+			self.start_preview()
+		keyboard.wait('esc')
+		self.stop_preview()
+		
+	def toggle_denoise(self):
+		if self.image_denoise:
+			self.image_denoise = False
+			print('\nTurning off denoising')
+		else:
+			self.image_denoise = True
+			print('\nTurning on denoising')
+
+	def increase_brightness(self, amt=brightness_info[-1]):
+		if self.brightness == brightness_info[1]:
+			return
+		else:
+			self.brightness += amt
+			print('\nBrightness = ' + str(self.brightness))
+	def decrease_brightness(self, amt=brightness_info[-1]):
+		if self.brightness == brightness_info[0]:
+			return
+		else:
+			self.brightness -= amt
+			print('\nBrightness = ' + str(self.brightness))
+	def reset_brightness(self):
+		self.brightness = brightness_info[2]
+		print('\nBrightness = ' + str(self.brightness))
+			
+	def increase_contrast(self, amt=contrast_info[-1]):
+		if self.contrast == contrast_info[1]:
+			return
+		else:
+			self.contrast += amt
+			print('\nContrast = ' + str(self.contrast))
+	def decrease_contrast(self, amt=contrast_info[-1]):
+		if self.contrast == contrast_info[0]:
+			return
+		else:
+			self.contrast -= amt
+			print('\nContrast = ' + str(self.contrast))
+	def reset_contrast(self):
+		self.contrast = contrast_info[2]
+		print('\nContrast = ' + str(self.contrast))
+					
+	def increase_saturation(self, amt=saturation_info[-1]):
+		if self.saturation == saturation_info[1]:
+			return
+		else:
+			self.saturation += amt
+			print('\nSaturation = ' + str(self.saturation))
+	def decrease_saturation(self, amt=saturation_info[-1]):
+		if self.saturation == saturation_info[0]:
+			return
+		else:
+			self.saturation -= amt
+			print('\nSaturation = ' + str(self.saturation))
+	def reset_saturation(self):
+		self.saturation = saturation_info[2]		
+		print('\nSaturation = ' + str(self.saturation))
+
+	def increase_sharpness(self, amt=sharpness_info[-1]):
+		if self.sharpness == sharpness_info[1]:
+			return
+		else:
+			self.sharpness += amt
+			print('\nSharpness = ' + str(self.sharpness))
+	def decrease_sharpness(self, amt=sharpness_info[-1]):
+		if self.sharpness == sharpness_info[0]:
+			return
+		else:
+			self.sharpness -= amt
+			print('\nSharpness = ' + str(self.sharpness))
+	def reset_sharpness(self):
+		self.sharpness = sharpness_info[2]
+		print('\nSharpness = ' + str(self.sharpness))
+			
+	def set_iso(self, value):
+		self.iso = value
+		if value==0:
+			print('\nISO set to automatic')
+		else:
+			print('\nISO = '+str(self.iso))
+		
+	def set_meter_mode(self, value):
+		self.meter_mode = value
+		print('\nMetering mode = ' + self.meter_mode)
+	
+	def set_exposure_mode(self, value):
+		self.exposure_mode = value
+		print('\nExposure mode = ' + self.exposure_mode)
+		
+	def increase_exposure(self, amt=exposure_info[-1]):
+		if self.exposure_compensation == exposure_info[1]:
+			return
+		else:
+			self.exposure_compensation += amt
+			print('\nExposure compensation = ' + str(self.exposure_compensation))
+	def decrease_exposure(self, amt=exposure_info[-1]):
+		if self.exposure_compensation == exposure_info[0]:
+			return
+		else:
+			self.exposure_compensation -= amt
+			print('\nExposure compensation = ' + str(self.exposure_compensation))
+	def reset_exposure(self):
+		self.exposure_compensation = exposure_info[2]
+		print('\nExposure compensation = ' + str(self.exposure_compensation))
+	
+	def increase_shutter_speed(self, amt):
+		# ~ print('stepsize is '+str(amt))
+		# ~ print('max is '+str(self.shutter_speed_max))
+		if not self.shutter_speed==0:
+			if self.shutter_speed+amt >= self.shutter_speed_max:
+				return
+			else:
+				self.shutter_speed += amt
+				print('\nShutter speed set to '+str(self.shutter_speed))
+	def decrease_shutter_speed(self, amt):
+		# ~ print('stepsize is '+str(amt))
+		# ~ print('max is '+str(self.shutter_speed_max))
+		if not self.shutter_speed==0:
+			if self.shutter_speed-amt <= 1000000./self.framerate:
+				return
+			else:
+				self.shutter_speed -= amt
+				print('\nShutter speed set to '+str(self.shutter_speed))
+	def reset_shutter_speed(self):
+		self.shutter_speed = self.shutter_speed_default
+		print('\nShutter speed set to '+str(self.shutter_speed))
+	def toggle_auto_shutter_speed(self):
+		if self.shutter_speed:
+			self.shutter_speed = 0
+			print('\nShutter speed set to automatic')
+		else:
+			self.shutter_speed = self.exposure_speed
+			print('\nShutter speed set to '+str(self.shutter_speed))
+
+	def toggle_awb(self):
+		if self.awb_mode == 'off':
+			self.awb_mode = 'auto'
+			print('\nAuto White Balance on')
+		elif self.awb_mode == 'auto':
+			self.awb_mode = 'off'
+			print('\nAuto White Balance off')
+
+	def increase_red_awb(self, amt=awb_gains_red_info[-1]):
+		if self.awb_mode=='off':
+			current_awb_gains = self.awb_gains
+			if current_awb_gains[0] == awb_gains_red_info[1]:
+				return
+			else:
+				new_red_awb = current_awb_gains[0] + amt
+				self.awb_gains = (new_red_awb, current_awb_gains[1])
+				print('\nAuto White Balance Gain (RED) = '+str(new_red_awb))			
+	def decrease_red_awb(self, amt=awb_gains_red_info[-1]):
+		if self.awb_mode=='off':
+			current_awb_gains = self.awb_gains
+			if current_awb_gains[0] == awb_gains_red_info[0]:
+				return
+			else:
+				new_red_awb = current_awb_gains[0] - amt
+				self.awb_gains = (new_red_awb, current_awb_gains[1])
+				print('\nAuto White Balance Gain (RED) = '+str(new_red_awb))
+	def reset_red_awb(self):
+		if self.awb_mode=='off':
+			self.awb_gains = (awb_gains_red_info[2], self.awb_gains[1])
+			print('\nAuto White Balance Gain (RED) = '+str(awb_gains_red_info[2]))		
+	
+	def increase_blue_awb(self, amt=awb_gains_blue_info[-1]):
+		if self.awb_mode=='off':		
+			current_awb_gains = self.awb_gains
+			if current_awb_gains[1] == awb_gains_blue_info[1]:
+				return
+			else:
+				new_blue_awb = current_awb_gains[1] + amt
+				self.awb_gains = (current_awb_gains[0], new_blue_awb)			
+				print('\nAuto White Balance Gain (BLUE) = '+str(new_blue_awb))		
+	def decrease_blue_awb(self, amt=awb_gains_blue_info[-1]):
+		if self.awb_mode=='off':
+			current_awb_gains = self.awb_gains
+			if current_awb_gains[1] == awb_gains_blue_info[0]:
+				return
+			else:
+				new_blue_awb = current_awb_gains[1] - amt
+				self.awb_gains = (current_awb_gains[0], new_blue_awb)
+				print('\nAuto White Balance Gain (BLUE) = '+str(new_blue_awb))		
+	def reset_blue_awb(self):
+		if self.awb_mode=='off':
+			self.awb_gains = (self.awb_gains[0], awb_gains_blue_info[2])
+			print('\nAuto White Balance Gain (BLUE) = '+str(awb_gains_blue_info[2]))				
+#TODO: link cropping with bounding box UI input
---
+++ ZionEvents.py
@@ -0,0 +1,87 @@
+from time import sleep
+from operator import itemgetter
+import keyboard
+
+def initializeSession():
+	# Checks timing parameters:
+	check_led_timings(Blue_Timing, Orange_Timing, UV_Timing)
+	myEventList = create_event_list(Blue_Timing, Orange_Timing, UV_Timing, Camera_Capture_Times, Repeat_N)
+	# Creates GPIO controller:
+	myGPIO = ZionGPIO()
+	# Creates camera:
+	myCamera = ZionCamera(Spatial_Res, Frame_Rate, ISO, Exposure_Mode, Shutter_Speed, Auto_White_Balance, Stabilize, gpio_ctrl=myGPIO)
+	return (myGPIO, myCamera, myEventList)
+
+#Check for well-formed timing arrays:
+def check_led_timings(LED_Blu_Timing, LED_Or_Timing, LED_UV_Timing, UV_duty_cycle=3.0):
+	for led_array in [LED_Blu_Timing, LED_Or_Timing, LED_UV_Timing]:
+		for onOffPair in led_array:
+			if len(onOffPair) != 2:
+				raise  ValueError('On-Off Pair ' + str(onOffPair) + ' must have length 2!')
+			elif onOffPair[1] <= onOffPair[0]:
+				raise ValueError('On-Off Pair must be in increasing order (ie time)!')
+	if not UV_duty_cycle is None:
+		for i in range(len(LED_UV_Timing)-1):
+			t_on1 = LED_UV_Timing[i][0]
+			t_off1 = LED_UV_Timing[i][1]
+			t_dc_on = t_off1-t_on1
+			t_on2 = LED_UV_Timing[i+1][0]
+			t_dc_off = t_on2 - t_off1
+			if t_dc_off < t_dc_on*(100.0-UV_duty_cycle)/UV_duty_cycle:
+				raise ValueError('UV timing must have a maximum duty cycle of 3%!')
+		#returns last t_on_dc so that we wait that long at the end of the event list (repeat or not)
+		# ~ return LED_UV_Timing[-1][1]-LED_UV_Timing[-1][0]
+
+def create_event_list(LED_Blu_Timing, LED_Or_Timing, LED_UV_Timing, Camera_Capture_Times, Repeat_N=0):
+	#first take last UV time on to create refactory period:
+	uv_time_on = LED_UV_Timing[-1][1]-LED_UV_Timing[-1][0]
+	
+	#First we create all events (we'll sort later):
+	eventList = []
+	eventList += [(time_pair[0], 'led_on', 'Blue') for time_pair in LED_Blu_Timing]
+	eventList += [(time_pair[1], 'led_off', 'Blue', time_pair[1]) for time_pair in LED_Blu_Timing]
+	eventList += [(time_pair[0], 'led_on', 'Orange', time_pair[0]) for time_pair in LED_Or_Timing]
+	eventList += [(time_pair[1], 'led_off', 'Orange', time_pair[1]) for time_pair in LED_Or_Timing]
+	eventList += [(time_pair[0], 'led_on', 'UV', time_pair[0]) for time_pair in LED_UV_Timing]
+	eventList += [(time_pair[1], 'led_off', 'UV', time_pair[1]) for time_pair in LED_UV_Timing]
+	eventList += [(capture_event[0], 'take_snapshot', capture_event[1]) for capture_event in Camera_Capture_Times]	
+	
+	#Now sort by time:
+	eventList.sort(key=itemgetter(0))
+	
+	# TODO: add refractory period?
+	return eventList
+	
+def performEvent(event, camera, gpio_ctrl, capture_file_name):
+	event_type=event[1]
+	if event_type == 'take_snapshot':
+		if event[2]:
+			camera.capture(capture_file_name, cropping=event[2], useIndex=True)
+		else:
+			camera.capture(capture_file_name, useIndex=True)			
+	elif event_type == 'led_off':
+		gpio_ctrl.turn_off_led(event[2])
+	elif event_type == 'led_on':
+		gpio_ctrl.turn_on_led(event[2])
+	elif event == 'wait':
+		pass
+	else:
+		#This is impossible to happen, but putting it here just in case
+		raise ValueError('Unknown Event Type!')
+	
+def performEventList(eventList, camera, gpio_ctrl, Repeat_N=0, baseFilename='capture_file_'):
+	saved_capture_idx = camera.file_idx
+	for n in range(Repeat_N+1):
+		#initial sleep (ie before first listed event
+		sleep(eventList[0][0]/1000.)
+		#now perform each event (except last, that'll be done later), and if it's a capture, increment saved_capture_idx
+		for e in range(len(eventList)-1):
+			event = eventList[e]
+			performEvent(event, camera, gpio_ctrl, baseFilename)
+			sleep((eventList[e+1][0]-event[0])/1000.)
+		#after for loop, now perform last event:
+		performEvent(eventList[-1], camera, gpio_ctrl, baseFilename)
+	
+def print_eventList(eventList): #for testing
+	for event in eventList:
+		print(event)
