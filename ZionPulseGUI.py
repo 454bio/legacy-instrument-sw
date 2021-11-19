aa5865aab36b5518248e3462e66376da6030402c
zach-detect <zaugenfeld@detect.com>
2021-11-19 17:27:30 -0500

    more colors

--- ZionPulseGUI.py
+++ ZionPulseGUI.py
@@ -3,7 +3,7 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
 from gi.repository import Gtk, GObject, Gst
 
-colors = ['None', 'UV', 'Blue', 'Orange']
+colors = ['None', 'UV', 'Blue', 'Orange', 'Red', 'Yellow', 'Green', 'Indigo', 'Violet']
         
 class TrashButton(Gtk.Button):
     def __init__(self, *args):
@@ -11,12 +11,45 @@ class TrashButton(Gtk.Button):
         img = Gtk.Image.new_from_icon_name("edit-delete-symbolic", 4)
         self.add(img)
         
-class LEDColorComboBox(Gtk.ComboBoxText):
+# ~ class LEDColorComboBox(Gtk.ComboBoxText):
+    # ~ def __init__(self, *args):
+        # ~ super(LEDColorComboBox,self).__init__(*args)
+        # ~ for color in colors:
+            # ~ self.append(None, color)
+        # ~ super(LEDColorComboBox,self).set_active(0)
+        
+class ColorCheckButton(Gtk.HBox):
+    def __init__(self, label, *args):
+        super(ColorCheckButton,self).__init__(*args)
+        self.label = label
+        self.pack_start( Gtk.CheckButton(), False, False, 0 )
+        self.pack_start( Gtk.Label(label), False, False, 0 )
+
+class LEDColorComboBox(Gtk.Grid):
     def __init__(self, *args):
         super(LEDColorComboBox,self).__init__(*args)
-        for color in colors:
-            self.append(None, color)
-        super(LEDColorComboBox,self).set_active(0)
+        self.ColorButtons = []
+        color = 0
+        for i in range(2):
+            for j in range(4):
+                color += 1
+                self.ColorButtons.append( ColorCheckButton(colors[color]) )
+                self.attach(self.ColorButtons[-1], i, j, 1, 1)
+                
+        # ~ self.attach(ColorCheckButton(),0,0,1,1)
+        # ~ self.attach(ColorCheckButton('Blue'),0,1,1,1)
+        # ~ self.attach(ColorCheckButton('Orange'),0,2,1,1)
+        # ~ self.attach(ColorCheckButton('Red'),0,3,1,1)
+        # ~ self.attach(ColorCheckButton('Green'),1,0,1,1)
+        # ~ self.attach(ColorCheckButton('Yellow'),1,1,1,1)
+        # ~ self.attach(ColorCheckButton('Indigo'),1,2,1,1)
+        # ~ self.attach(ColorCheckButton('Violet'),1,3,1,1)
+            
+    def get_active_colors(self):
+        colors_selected = []
+        for color_button in self.ColorButtons:
+            if color_button.get_active():
+                colors_selected.append( color_button.label )
 
 class EventEntry(Gtk.HBox): 
     def __init__(self, parent, *args):
@@ -25,8 +58,10 @@ class EventEntry(Gtk.HBox):
         self.ColorComboBox = LEDColorComboBox()
         self.TimeEntry = Gtk.Entry()
         self.TimeEntry.set_width_chars(14)
+        self.TimeEntry.set_valign(3)
         self.DutyCycleEntry = Gtk.Entry()
         self.DutyCycleEntry.set_width_chars(3)
+        self.DutyCycleEntry.set_valign(3)
         self.CaptureToggleButton = Gtk.CheckButton()
         self.DeleteButton = TrashButton()
         self.pack_start( self.DeleteButton, False, False, 0)
@@ -35,28 +70,17 @@ class EventEntry(Gtk.HBox):
         self.pack_start( self.DutyCycleEntry, False, False, 0)
         self.pack_start( self.CaptureToggleButton, False, False, 0)
         self.DeleteButton.connect("clicked", self.on_event_delete_button)
-        self.ColorComboBox.connect("changed", self.on_color_changed)
-        self.CaptureToggleButton.set_active(False)
-        self.CaptureToggleButton.set_sensitive(False)
-        self.DutyCycleEntry.set_sensitive(False)
+        # ~ self.ColorComboBox.connect("changed", self.on_color_changed)
+        # ~ self.CaptureToggleButton.set_active(False)
+        # ~ self.CaptureToggleButton.set_sensitive(False)
+        # ~ self.DutyCycleEntry.set_sensitive(False)
 
     def on_event_delete_button(self, button):
         idx = self.parent.EventEntries.index(self)
         self.destroy()
         # ~ print('idx to remove = '+str(idx))
         del(self.parent.EventEntries[idx])
-    
-    def on_color_changed(self, combo):
-        active_idx = combo.get_active()
-        if not active_idx:
-            self.DutyCycleEntry.set_sensitive(False)
-            self.CaptureToggleButton.set_active(False)
-            self.CaptureToggleButton.set_sensitive(False)
-        else:
-            self.DutyCycleEntry.set_sensitive(True)
-            self.CaptureToggleButton.set_active(True)
-            self.CaptureToggleButton.set_sensitive(True)
-            
+
     def exportEvent(self):
         color_idx = self.ColorComboBox.get_active()
         time = self.TimeEntry.get_text()
