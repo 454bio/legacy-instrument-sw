07d688b880a96ed62ee1db9a6401995adbab50b4
zaugenfeld <zaugenfeld@gmail.com>
2021-12-29 07:32:29 -0500

    added celery

--- ZionBluetooth.py
+++ ZionBluetooth.py
@@ -5,10 +5,16 @@ import dbus
 from BTAdvertisement import Advertisement
 from BTService import Application, Service, Characteristic, Descriptor
 
-from gpiozero import CPUTemperature
+from ZionCelery import zion_report
 
 GATT_CHRC_IFACE = "org.bluez.GattCharacteristic1"
 NOTIFY_TIMEOUT = 5000
+
+STATUS_ERROR = 0 #failure
+STATUS_PENDING = 1
+STATUS_BUSY = 2 #started
+STATUS_READY = 3 #success
+STATUS_UNKNOWN = 4 #revoked? retry?
 
 class ZionAdvertisement(Advertisement):
     def __init__(self, index):
@@ -22,12 +28,20 @@ class ZionService(Service):
     def __init__(self, index):
         super(ZionService, self).__init__(index, self.SVC_UUID, True)
 
-        self.add_characteristic(ReportCharacteristic(self))
+        self._report_result = None
+
         self.add_characteristic(ParameterCharacteristic(self))
         self.add_characteristic(ProtocolCharacteristic(self))
-
         self.ParameterFile = 'default_parameter_file.txt'
         self.ProtocolFile = 'default_protocol_file.txt'
+
+        self.add_characteristic(ReportStatusCharacteristic(self))
+        self.ReportStatus = STATUS_INIT
+
+        self.add_characteristic(ReportCharacteristic(self))
+        self.add_characteristic(ReportFileCharacteristic(self))
+        self.ReportFilename = 'default_report_filename.txt'
+        
 
     def get_parameter_file(self):
         return self.ParameterFile
@@ -35,12 +49,103 @@ class ZionService(Service):
     def get_protocol_file(self):
         return self.ProtocolFile
 
+    def get_report_filename(self):
+        return self.ReportFilename
+
     def set_parameter_file(self, filename):
         self.ParameterFile = filename
 
     def set_protocol_file(self, filename):
         self.ProtocolFile = filename
 
+    def get_report_filename(self, filename):
+        self.ReportFilename = filename
+
+    def update_status(self):
+        if self._report_result is not None:
+            status = self._report_result.status
+            if status=='PENDING':
+                self.ReportStatus = STATUS_INIT
+            elif status=='STARTED':
+                self.ReportStatus = STATUS_BUSY
+            elif status=='SUCCESS':
+                self.ReportStatus = STATUS_READY
+            elif status=='FAILURE':
+                self.ReportStatus = STATUS_ERROR
+            else:
+                self.ReportStatus = STATUS_UNKNOWN
+        else:
+            status = 'UNDEFINED'
+        return status
+
+    def generate_report(self):
+        args = (self.ParameterFile,
+                 self.ProtocolFile, 
+                 self.ReportFilename)
+        self._report_result = zion_report.apply_async(args, time_limit = 60)
+
+
+class ReportStatusCharacteristic(Characteristic):
+    UUID = "00000003-710e-4a5b-8d75-3e5b444bc3cf"
+
+    def __init__(self, service):
+        self.notifying = False
+
+        super(ReportStatusCharacteristic, self).__init__(
+                self.UUID,
+                ["notify", "read"], service)
+        self.add_descriptor(ReportStatusDescriptor(self))
+
+    def get_status(self):
+        value = []
+        status = self.service.update_status()
+        for c in status:
+            value.append(dbus.Byte(c.encode()))
+        return value
+
+    def set_status_callback(self):
+        if self.notifying:
+            old_state = self.service.get_report_status()
+            status = self.get_status()
+            if not self.serivce.get_report_status() == old_state:
+                self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": status}, [])
+        return self.notifying
+
+    def StartNotify(self):
+        if self.notifying:
+            return
+        self.notifying = True
+        old_state = self.service.get_report_status()
+            status = self.get_status()
+            if not self.serivce.get_report_status() == old_state:
+                self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": status}, [])
+        self.add_timeout(NOTIFY_TIMEOUT, self.set_status_callback)
+
+    def StopNotify(self):
+        self.notifying = False
+
+    def ReadValue(self, options):
+        status = self.get_status()
+
+        return status
+
+class ReportStatusDescriptor(Descriptor):
+    UUID = "2901"
+    DESCRIPTOR_VALUE = "Report Status"
+
+    def __init__(self, characteristic):
+        super(ReportStatusDescriptor, self).__init__(
+                self.UUID,
+                ["read"],
+                characteristic)
+
+    def ReadValue(self, options):
+        value = []
+        desc = self.DESCRIPTOR_VALUE
+
+        for c in desc:
+            value.append(dbus.Byte(c.encode()))
+        return value
 
 class ReportCharacteristic(Characteristic):
     UUID = "00000002-710e-4a5b-8d75-3e5b444bc3cf"
@@ -50,43 +155,22 @@ class ReportCharacteristic(Characteristic):
 
         super(ReportCharacteristic, self).__init__(
                 self.UUID,
-                ["notify", "read"], service)
+                ["read"], service)
         self.add_descriptor(ReportDescriptor(self))
 
-    def get_temperature(self):
+    def get_report(self):
         value = []
-
-        cpu = CPUTemperature()
-        temp = cpu.temperature
-
-        strtemp = str(round(temp, 1)) + " C"
+        try:
+            with open(self.ReportFilename, 'r') as reportFile:
+                strtemp = reportFile.read()
+        except FileError:
+            strtemp=''
         for c in strtemp:
             value.append(dbus.Byte(c.encode()))
         return value
 
-    def set_temperature_callback(self):
-        if self.notifying:
-            value = self.get_temperature()
-            self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": value}, [])
-
-        return self.notifying
-
-    def StartNotify(self):
-        if self.notifying:
-            return
-
-        self.notifying = True
-
-        value = self.get_temperature()
-        self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": value}, [])
-        self.add_timeout(NOTIFY_TIMEOUT, self.set_temperature_callback)
-
-    def StopNotify(self):
-        self.notifying = False
-
     def ReadValue(self, options):
-        value = self.get_temperature()
-
+        value = self.get_report()
         return value
 
 class ReportDescriptor(Descriptor):
@@ -108,7 +192,7 @@ class ReportDescriptor(Descriptor):
         return value
 
 class ParameterCharacteristic(Characteristic):
-    UUID = "00000003-710e-4a5b-8d75-3e5b444bc3cf"
+    UUID = "00000004-710e-4a5b-8d75-3e5b444bc3cf"
 
     def __init__(self, service):
         super(ParameterCharacteristic, self).__init__(
@@ -126,7 +210,6 @@ class ParameterCharacteristic(Characteristic):
         for c in val:
             value.append(dbus.Byte(c.encode()))
         return value
-
 
 class ParameterDescriptor(Descriptor):
     UUID = "2901"
@@ -146,7 +229,7 @@ class ParameterDescriptor(Descriptor):
         return value
 
 class ProtocolCharacteristic(Characteristic):
-    UUID = "00000004-710e-4a5b-8d75-3e5b444bc3cf"
+    UUID = "00000005-710e-4a5b-8d75-3e5b444bc3cf"
 
     def __init__(self, service):
         super(ProtocolCharacteristic, self).__init__(
@@ -157,6 +240,9 @@ class ProtocolCharacteristic(Characteristic):
     def WriteValue(self, value, options):
          val = value
          self.service.set_protocol_file(val)
+         
+         #now trigger generating a new report:
+         self.service.generate_report()
 
     def ReadValue(self, options):
         value = []
@@ -165,13 +251,49 @@ class ProtocolCharacteristic(Characteristic):
             value.append(dbus.Byte(c.encode()))
         return value
 
-
 class ProtocolDescriptor(Descriptor):
     UUID = "2902"
     DESCRIPTOR_VALUE = "Protocol File"
 
     def __init__(self, characteristic):
         super(ProtocolDescriptor, self).__init__(
+                self.UUID,
+                ["read"],
+                characteristic)
+
+    def ReadValue(self, options):
+        value = []
+        desc = self.DESCRIPTOR_VALUE
+        for c in desc:
+            value.append(dbus.Byte(c.encode()))
+        return value
+
+class ReportFileCharacteristic(Characteristic):
+    UUID = "00000006-710e-4a5b-8d75-3e5b444bc3cf"
+
+    def __init__(self, service):
+        super(ReportFileCharacteristic, self).__init__(
+                self.UUID,
+                ["read", "write"], service)
+        self.add_descriptor(ReportFileDescriptor(self))
+
+    def WriteValue(self, value, options):
+         val = value
+         self.service.set_report_filename(val)
+
+    def ReadValue(self, options):
+        value = []
+        val = self.service.get_report_filename()
+        for c in val:
+            value.append(dbus.Byte(c.encode()))
+        return value
+
+class ReportFileDescriptor(Descriptor):
+    UUID = "2902"
+    DESCRIPTOR_VALUE = "Report Filename"
+
+    def __init__(self, characteristic):
+        super(ReportFileDescriptor, self).__init__(
                 self.UUID,
                 ["read"],
                 characteristic)
---
+++ ZionCelery.py
@@ -0,0 +1,31 @@
+import time
+from celery import Celery
+
+class ZionCelery(Celery):
+    pass
+
+zion_celery = Celery('ZionCelery', backend='redis://localhost', broker='pyamqp://')
+
+@zion_celery.task(name='zion-report', bind=True, track_started=True)
+#def zion_report(params, protocol, report_filename, timeout=20):
+#    time.sleep(timeout)
+def zion_report(self, params, protocol, report_filename):
+    ret = 0
+    time.sleep(20)
+    with open(report_filename, 'w') as file:
+        file.writelines(['This is a Zion Report in '+report_filename,
+                         'Parameters from: '+parameter_file,
+                         'Protocol from: '+protocol_file])
+    ret = 1
+    return ret
+
+#celery -A ZionCelery worker --loglevel=info
+
+ #must eventually call get() or forget() on EVERY AsyncResult instance returned after calling a task.
+
+#report.status:
+#PENDING
+#STARTED
+#RETRY (disable)
+#FAILURE 
+#SUCCESS
