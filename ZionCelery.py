2abfdb4f683c26991d8ff8b36e99ffe4240ad6f1
zach-detect <zaugenfeld@detect.com>
2021-12-30 15:55:10 -0500

    celery with rabbbitmq and redis work now

--- ZionCelery.py
+++ ZionCelery.py
@@ -1,28 +1,35 @@
 import time
 from celery import Celery
 
-zion_celery = Celery('ZionCelery', backend='redis://localhost', broker='pyamqp://')
+zion_celery = Celery('ZionCelery', broker='pyamqp://', backend='redis://localhost:6379/0')
 
 #celery -A ZionCelery worker --loglevel=info
 
 @zion_celery.task(name='zion-report-spoofed', bind=True, track_started=True)
-def spoof_zion_report(params, protocol, report_filename, timeout=20):
+def spoof_zion_report(self, parameter_file, protocol_file, report_filename, timeout=20):
     ret = 0
     time.sleep(timeout)
     with open(report_filename, 'w') as file:
-        file.writelines(['This is a Zion Report!',
-                         'Parameters from: '+parameter_file,
+        file.writelines(['This is a Zion Report!\n',
+                         'Parameters from: '+parameter_file+'\n',
                          'Protocol from: '+protocol_file])
     ret = 1
     return ret
 
-@zion_celery.task(name='zion-report-spoofed', bind=True, track_started=True)
-def zion_report(params, protocol, report_filename, timeout=20):
+@zion_celery.task(name='zion-report', bind=True, track_started=True)
+def zion_report(self, parameter_file, protocol_file, report_filename, timeout=20):
     ret = 0
     time.sleep(timeout)
     with open(report_filename, 'w') as file:
-        file.writelines(['This is a Zion Report!',
-                         'Parameters from: '+parameter_file,
+        file.writelines(['This is a Zion Report!\n',
+                         'Parameters from: '+parameter_file+'\n',
                          'Protocol from: '+protocol_file])
     ret = 1
     return ret
+
+@zion_celery.task(bind=True, track_started=True)
+def test_celery(self,x,y):
+    return x+y
+    
+if __name__=='__main__':
+    zion_celery.start()
