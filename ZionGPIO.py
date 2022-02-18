170373bb3c69204b5df331f1247cdcfa4477a2a2
Brett Gyarfas <bgyarfas@tesseracthealth.com>
2022-02-18 18:08:42 -0500

    Refactoring protocols to support nested groups of events (no limit to number of nestings)  Protocols now consist of a list of "ZionEventGroup". (Version 2)  Each event group has a list of either a "ZionEvent" or another "ZionEventGroup".  - Added Version to protocol  - Added name to an eventGroup (not supported in the GUI yet)  - Depreciated: Interrepeat_Delay. Use a non-capture event if you'd like to add a wait.  - Version 1 protocols are automatically converted to Version 2  - Version 2 protocols are flattened by default (until GUI support for nested groups is added) so nested protocols can be run in the GUI

--- ZionGPIO.py
+++ ZionGPIO.py
@@ -1,10 +1,11 @@
 import pigpio
-import os
-import itertools
 import glob
 import time
-import threading
 from functools import partial
+from typing import List, Dict
+import threading
+
+from ZionEvents import ZionEvent, ZionLED, ZionLEDColor
 
 # Gpio Pin Lookup Table. Index is GPIO #, format is (pin #, enabled, alternate function)
 # (can remove/trim if memory is an issue)
@@ -61,16 +62,23 @@ FSTROBE = 23 #pin 16
 XVS = 24 #pin 18
 
 class ZionGPIO(pigpio.pi):
-	
-	UV_idx = 0
-	BLUE_idx = 1
-	ORANGE_idx = 2
-	
+
+	LED_IDX : Dict[ZionLEDColor, int] = {
+		ZionLEDColor.UV : 0,
+		ZionLEDColor.BLUE : 1,
+		ZionLEDColor.ORANGE : 2,
+	}
+
+	LED_DC : Dict[ZionLEDColor, int] = {
+		ZionLEDColor.UV : 100,
+		ZionLEDColor.BLUE : 100,
+		ZionLEDColor.ORANGE : 100,
+	}
+
 	#TODO: add UV off command to appropriate error handles?
-	
 	def __init__(self, pwm_freq, UV_gpios=UV, Blue_gpios=BLUE, Orange_gpios=ORANGE, temp_out_gpio=TEMP_OUTPUT, temp_in_gpio=TEMP_INPUT_1W, camera_trigger_gpio=CAMERA_TRIGGER, parent=None):
 		super(ZionGPIO, self).__init__()
-		
+
 		self.parent=parent
 		self.frequency = pwm_freq #not changing for different gpios
 		self.micros = 1000000./self.frequency #period in microseconds
@@ -85,52 +93,41 @@ class ZionGPIO(pigpio.pi):
 					super(ZionGPIO,self).set_PWM_range(g, 100)
 			else:
 				raise ValueError('Chosen GPIO is not enabled!')
-		
+
 		# Now make camera sync signals inputs:
 		for g in [FSTROBE, XVS]:
 			if GpioPins[g][1]:
 				super(ZionGPIO,self).set_mode(g, pigpio.INPUT)
-		
-		self.gpioList = [UV_gpios, Blue_gpios, Orange_gpios] #Order is important
-		self.pS = [0.0]*3 #UV, Blue, Orange
-		self.dc = [0.0]*3
+
+		self.gpioList = {
+			ZionLEDColor.UV: UV_gpios,
+			ZionLEDColor.BLUE: Blue_gpios,
+			ZionLEDColor.ORANGE: Orange_gpios
+		}
+
+		self.pS = {c: 0.0 for c in ZionLEDColor} #UV, Blue, Orange
+		self.dc = {c: 0.0 for c in ZionLEDColor}
 		self.old_wid = None
-		self.stop = False
-		
-		self.Blue_DC = 100
-		self.Orange_DC = 100
-		self.UV_DC = 100
-		
-		#Now set up register methods of setting/clearing led outputs:		
-		# (not used for pwm)
-		# ~ self.UV_Reg = 0
-		# ~ for bit in UV_gpios:
-			# ~ self.UV_Reg |= (1<<bit)
-		# ~ self.Blue_Reg = 0
-		# ~ for bit in Blue_gpios:
-			# ~ self.Blue_Reg |= (1<<bit)
-		# ~ self.Orange_Reg = 0
-		# ~ for bit in Orange_gpios:
-			# ~ self.Orange_Reg |= (1<<bit)
-		
+		self.stop_event = threading.Event()
+
 		self.Camera_Trigger = camera_trigger_gpio
-        
+
 		self.Temp_Output_GPIO = temp_out_gpio
 		#TODO: implement heat control output
-        
-        #No check for Temperature Input GPIO pin, this is done in boot config file (including GPIO choice)
+
+		#No check for Temperature Input GPIO pin, this is done in boot config file (including GPIO choice)
 		base_dir = '/sys/bus/w1/devices/'
 		try:
 			self.Temp_1W_device = glob.glob(base_dir + '28*')[0]
 		except IndexError:
 			print('Warning: 1-Wire interface not connected.')
-			self.Temp_1W_device = None 
-		
+			self.Temp_1W_device = None
+
 		# Last thing is to ensure all gpio outputs are off:
-		for color in range(3):
+		for color in ZionLEDColor:
 			self.enable_led(color, 0)
 		self.camera_trigger(False)
-		
+
 		self.test_delay = 0
 		self.test_pulse_width = 1
 
@@ -151,26 +148,21 @@ class ZionGPIO(pigpio.pi):
 			return temp_c
 		else:
 			return None
-			
-	def set_pulse_start_in_micros(self, color, start):
+
+	def set_pulse_start_in_micros(self, color : ZionLEDColor, start : int):
 		start %= self.micros
 		self.pS[color] = start / self.micros
-	
-	def set_duty_cycle(self, color, dc):
+
+	def set_duty_cycle(self, color : ZionLEDColor, dc : float):
 		self.dc[color] = dc
-		if dc>0:
-			if color==ZionGPIO.UV_idx:
-				self.UV_DC = int(dc*100)
-			elif color==ZionGPIO.BLUE_idx:
-				self.Blue_DC = int(dc*100)
-			elif color==ZionGPIO.ORANGE_idx:
-				self.Orange_DC = int(dc*100)
+		if dc > 0:
+			self.LED_DC[color] = int(dc * 100)
 
 	def update_pwm_settings(self):
 
 		null_wave = True
-		for color in range(len(self.gpioList)):
-			for g in self.gpioList[color]:
+		for color, gpios in self.gpioList.items():
+			for g in gpios:
 				null_wave = False
 				on = int(self.pS[color] * self.micros)
 				length = int(self.dc[color] * self.micros)
@@ -194,7 +186,7 @@ class ZionGPIO(pigpio.pi):
 							pigpio.pulse(1<<g,    0, micros - on),
 						])
 		if not null_wave:
-			if not self.stop:
+			if not self.stop_event.is_set():
 				new_wid = self.wave_create()
 				if self.old_wid is not None:
 					self.wave_send_using_mode(new_wid, pigpio.WAVE_MODE_REPEAT_SYNC)
@@ -204,82 +196,48 @@ class ZionGPIO(pigpio.pi):
 				else:
 					self.wave_send_repeat(new_wid)
 				self.old_wid = new_wid
-				
+
 	def cancel_PWM(self):
-		for color in range(3):
+		for color in ZionLEDColor:
 			self.set_duty_cycle(color, 0)
 		self.update_pwm_settings()
-		# ~ self.stop = True
-		# ~ self.wave_tx_stop()
-		# ~ if self.old_wid is not None:
-			# ~ self.wave_delete(self.old_wid)
-			
-	def enable_leds(self, dcDict, verbose=False):
-		for color in dcDict.keys():
-			self.enable_led(color, dcDict[color]/100, verbose=verbose, update=False)
-		self.update_pwm_settings()
-		
-	def disable_leds(self, dcDict, verbose=False):
-		for color in dcDict.keys():
-			self.enable_led(color, 0, verbose=verbose, update=False)
+
+	def enable_leds(self, leds : List[ZionLED], verbose=False):
+		for l in leds:
+			self.enable_led(l.color, l.intensity/100, verbose=verbose, update=False)
 		self.update_pwm_settings()
 
-	def enable_led(self, color, amt, verbose=False, update=True):
-		# ~ amt = amt
-		# ~ if amt<0 or amt>1:
-			# ~ raise ValueError("Duty Cycle must be between 0 and 1!")
-		if color=='UV':
-			self.set_duty_cycle(ZionGPIO.UV_idx, amt)
-			print('\nSetting UV to '+str(amt))
-			if verbose:
-				self.parent.gui.printToLog('UV set to '+str(amt))
-		if color=='Blue':
-			self.set_duty_cycle(ZionGPIO.BLUE_idx, amt)
-			print('\nSetting Blue to '+str(amt))
-			if verbose:
-				self.parent.gui.printToLog('Blue set to '+str(amt))
-		if color=='Orange':
-			self.set_duty_cycle(ZionGPIO.ORANGE_idx, amt)
-			print('\nSetting Orange to '+str(amt))
-			if verbose:
-				self.parent.gui.printToLog('Orange set to '+str(amt))
+	def disable_leds(self, leds : List[ZionLED], verbose=False):
+		for l in leds:
+			self.enable_led(l.color, 0, verbose=verbose, update=False)
+		self.update_pwm_settings()
+
+	def enable_led(self, color : ZionLEDColor, amt : float, verbose : bool = False, update: bool =True):
+		self.set_duty_cycle(color, amt)
+		print(f"\nSetting {color.name} to {amt}")
+		if verbose:
+			self.parent.gui.printToLog(f"{color.name} set to {amt}")
+
 		if update:
 			self.update_pwm_settings()
 
-	def turn_on_led(self, color, verbose=False):
-		if color=='UV':
-			amt = self.UV_DC/100.
-			self.set_duty_cycle(ZionGPIO.UV_idx, amt)
-			print('\nSetting UV to '+str(amt))
-			if verbose:
-				self.parent.gui.printToLog('UV set to '+str(amt))
-		if color=='Blue':
-			amt = self.Blue_DC/100.
-			self.set_duty_cycle(ZionGPIO.BLUE_idx, amt)
-			print('\nSetting Blue to '+str(amt))
-			if verbose:
-				self.parent.gui.printToLog('Blue set to '+str(amt))
-		if color=='Orange':
-			amt = self.Orange_DC/100.
-			self.set_duty_cycle(ZionGPIO.ORANGE_idx, amt)
-			print('\nSetting Orange to '+str(amt))
-			if verbose:
-				self.parent.gui.printToLog('Orange set to '+str(amt))
-		self.update_pwm_settings()
-		
-	def send_uv_pulse(self, pulsetime, dc):
-		self.enable_led('UV', dc)
-		time.sleep(pulsetime/1000.)
-		self.enable_led('UV', 0)
+	def turn_on_led(self, color : ZionLEDColor, verbose : bool = False):
+		amt = self.LED_DC[color] / 100.
+		self.set_duty_cycle(self.LED_IDX[color], amt)
+		print(f"\nSetting {color.name} to {amt}")
+		if verbose:
+			self.parent.gui.printToLog(f"{color.name} set to {amt}")
 
-	def enable_vsync_callback(self, colors, pw, capture, grp):
+		self.update_pwm_settings()
+
+	def send_uv_pulse(self, pulsetime : float, dc : int):
+		self.enable_led(ZionLEDColor.UV, dc)
+		time.sleep(pulsetime / 1000.)
+		self.enable_led(ZionLEDColor.UV, 0)
+
+	def enable_vsync_callback(self, event : ZionEvent):
 		self.callback_for_uv_pulse = self.callback(
 			XVS,
 			pigpio.RISING_EDGE,
-			partial(self.parent.pulse_on_trigger, colors, pw, capture, grp)
+			partial(self.parent.pulse_on_trigger, event)
 		)
-
-	# ~ def enable_vsync_callback(self):
-		# ~ self.callback_for_uv_pulse = super(ZionGPIO,self).callback(XVS, pigpio.RISING_EDGE, lambda gpio,level,ticks: self.uv_pulse_on_trigger(gpio, level, ticks))
-
-
