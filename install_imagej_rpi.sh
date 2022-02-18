9c6d98399c1d8eb5f0d511a721020bab51ac39dd
Brett Gyarfas <bgyarfas@tesseracthealth.com>
2022-02-18 17:56:29 -0500

    Fixed installer script

--- install_imagej_rpi.sh
+++ install_imagej_rpi.sh
@@ -4,17 +4,16 @@ set -Eeuo pipefail
 sudo apt-get -y update
 sudo apt-get -y install openjdk-8-jre
 sudo update-alternatives --skip-auto --config java
-wget https://wsr.imagej.net/distros/cross-platform/ij153.zip
-unzip ij153.zip -d ~/
-rm ij153.zip
+wget https://wsr.imagej.net/distros/cross-platform/ij153.zip -O /tmp/ij153.zip
+unzip -o /tmp/ij153.zip -d ~/
+rm /tmp/ij153.zip
 
 sed -i "s/quick_exec=0/quick_exec=1/" ~/.config/libfm/libfm.conf
-wget https://upload.wikimedia.org/wikipedia/commons/d/db/ImageJLogo.png -O ~/ImageJ/icon.png
 
 cat << 'EOF' > ~/ImageJ/run
 #!/bin/bash
 
-script_dir=$(cd \"$(dirname "${BASH_SOURCE[0]}\")\" &>/dev/null && pwd -P)
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 java -Xmx512m -jar /${script_dir}/ij.jar
 EOF
 
@@ -25,8 +24,8 @@ cat << 'EOF' > ~/Desktop/ImageJ.desktop
 Name=ImageJ
 Version=1.0
 Comment=Launches ImageJ
-Exec=/home/pi/Downloads/ImageJ/run
-Icon=/home/pi/Downloads/ImageJ/icon.png
+Exec=/home/pi/ImageJ/run
+Icon=/home/pi/ImageJ/icon.png
 Terminal=false
 Type=Application
 Categories=Education
@@ -34,3 +33,6 @@ StartupNotify=true
 EOF
 
 chmod +x ~/Desktop/ImageJ.desktop
+
+wget --timeout=3 https://imagej.nih.gov/ij/images/ImageJ.png -O ~/ImageJ/icon.png || \
+wget --timeout=3 https://upload.wikimedia.org/wikipedia/commons/d/db/ImageJLogo.png -O ~/ImageJ/icon.png
