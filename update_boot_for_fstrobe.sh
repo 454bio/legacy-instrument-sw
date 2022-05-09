a57f48fd8fa9ee991dd26a5f3a0e5587c5511d49
zach-detect <zaugenfeld@detect.com>
2022-05-09 15:23:54 -0400

    update for bluetooth switch gpio, slight edit to differentiate bootconfig change for hat board than fstrobe

--- update_boot_for_fstrobe.sh
+++ update_boot_for_fstrobe.sh
@@ -9,7 +9,6 @@ file=/boot/config.txt
 
 echo -n "Updating $file..."
 
-grep -q "^dtoverlay=w1-gpio" $file && sed -i.bak 's/^dtoverlay=w1-gpio.*/dtoverlay=w1-gpio/' $file || echo "dtoverlay=w1-gpio" >> $file
 grep -q "^gpu_mem=" $file && sed -i.bak 's/^gpu_mem=.*/gpu_mem=256/' $file || echo "gpu_mem=256" >> $file
 grep -q "^imx477_hv_sync_enable=" $file && sed -i.bak 's/^imx477_hv_sync_enable=.*/imx477_hv_sync_enable=1/' $file || echo "imx477_hv_sync_enable=1" >> $file
 grep -q "^imx477_vsync_polarity=" $file && sed -i.bak 's/^imx477_vsync_polarity=.*/imx477_vsync_polarity=1/' $file || echo "imx477_vsync_polarity=1" >> $file
@@ -19,6 +18,8 @@ grep -q "^imx477_fstrobe_cont_trig=" $file && sed -i.bak 's/^imx477_fstrobe_cont
 grep -q "^imx477_fstrobe_delay=" $file && sed -i.bak 's/^imx477_fstrobe_delay=.*/imx477_fstrobe_delay=3040/' $file || echo "imx477_fstrobe_delay=3040" >> $file
 grep -q "^imx477_fstrobe_stills_only=" $file && sed -i.bak 's/^imx477_fstrobe_stills_only=.*/imx477_fstrobe_stills_only=1/' $file || echo "imx477_fstrobe_stills_only=1" >> $file
 grep -q "^imx477_fstrobe_width=" $file && sed -i.bak 's/^imx477_fstrobe_width=.*/imx477_fstrobe_width=12000/' $file || echo "imx477_fstrobe_width=12000" >> $file
+
+grep -q "^dtoverlay=w1-gpio" $file && sed -i.bak 's/^dtoverlay=w1-gpio.*/dtoverlay=w1-gpio/' $file || echo "dtoverlay=w1-gpio" >> $file
 
 echo "Success!"
 echo ""
