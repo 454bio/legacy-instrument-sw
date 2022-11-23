16413df69991e388da02bb5b59360a0fb284a10a
zion454 <nas@454.bio>
2022-11-23 12:00:48 -0500

    zach driving while jose drivng him nuts

--- ZionSession.py
+++ ZionSession.py
@@ -295,7 +295,7 @@ class ZionSession():
                     start_fstrobe = self.GPIO.get_num_fstrobes()
                     capture_busy_event = self.GPIO.get_capture_busy_event()
                     bayer = True
-                    quality = 1
+                    quality = 85
                     for frame_ind, (event, _) in enumerate(zip(flat_events, self.Camera.capture_continuous(seq_stream, format='jpeg', burst=False, bayer=bayer, thumbnail=None, quality=quality))):
                         # print(f"stream_size: {seq_stream.tell()}")
                         # stream_size = seq_stream.tell()
