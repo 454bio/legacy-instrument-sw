3689eb150894acab3f8314adb985d68a478786f9
zion454 <nas@454.bio>
2022-12-22 18:40:39 -0500

    initial commit for multithreaded approach to automatically convert files written WIP

--- image_processing/ZionImage.py
+++ image_processing/ZionImage.py
@@ -1,7 +1,13 @@
 import os
+import multiprocessing
+import threading
 import numpy as np
 import pandas as pd
 import cv2
+from collections import UserDict
 from tifffile import imread, imwrite
 
 from raw_converter import jpg_to_raw
+
+class ZionImage(UserDict):
+	pass
