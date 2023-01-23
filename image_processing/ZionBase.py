2d141bf227341d1ea6ec0d9c770997ab755deefe
zion454 <nas@454.bio>
2023-01-22 23:16:51 -0500

    spot data extraction

--- image_processing/ZionBase.py
+++ image_processing/ZionBase.py
@@ -5,10 +5,11 @@ import pandas as pd
 from skimage.color import rgb2hsv
 from matplotlib import pyplot as plt
 
-from ZionImage import ZionImage
+# ~ from image_processing.ZionImage import ZionImage
 
 BASES = ('A', 'C', 'G', 'T')#, 'S') #todo: include scatter color as a base?
-df_cols = [ "wavelength",
+df_cols = [ "roi",
+            "wavelength",
             "mean_R",
             "mean_G",
             "mean_B",
@@ -37,19 +38,19 @@ df_cols = [ "wavelength",
 
 class ZionBase(UserString):
 
-	Names = ('A', 'C', 'G', 'T', '!')
+    Names = ('A', 'C', 'G', 'T', '!')
 
-	def __init__(self, char):
-		if not isinstance(char, str):
-			raise TypeError("Base must be a character!")
-		elif len(char) != 1:
-			raise ValueError("Base must be a one-character string!")
-		elif char not in ZionBase.Names:
-			raise ValueError(f"Base must be valid character (not '{char}')!")
-		else:
-			super().__init__(char)
+    def __init__(self, char):
+        if not isinstance(char, str):
+            raise TypeError("Base must be a character!")
+        elif len(char) != 1:
+            raise ValueError("Base must be a one-character string!")
+        elif char not in ZionBase.Names:
+            raise ValueError(f"Base must be valid character (not '{char}')!")
+        else:
+            super().__init__(char)
 
-def extract_spot_data(img:ZionImage, roi_labels:np.array, csvFileName = None):
+def extract_spot_data(img, roi_labels:np.array, csvFileName = None):
 #bUseMedian:bool = False, bUseHsv:bool = False, ):
     ''' takes in a ZionImage (ie a dict of RGB images) and a 3D image of spot labels. Optionally writes a csv file.
         Outputs a pandas dataframe containing all data.
@@ -57,29 +58,47 @@ def extract_spot_data(img:ZionImage, roi_labels:np.array, csvFileName = None):
 
     numSpots = np.max(roi_labels)
     df_total = pd.DataFrame()
-    spot_data = []
-    if csvFileName is not None:
-        with open(csvFileName, "w") as f:
-            #todo write csv labels
+    spot_data = dict()
+    # ~ if csvFileName is not None:
+        # ~ with open(csvFileName, "w") as f:
+            # ~ #todo write csv labels
+    pd_idx = 0
     for s_idx in range(1,numSpots+1): #spots go from [1, numSpots]
-        spot_data = [f"spot_{s_idx:03d}"]
+        # ~ spot_data = [f"spot_{s_idx:03d}"]
         for w in img.wavelengths:
-            spot_data.append(w)
+            spot_data[df_cols[0]] = f"spot_{s_idx:03d}"
+            spot_data[df_cols[1]] = w
             hsv_intensities = rgb2hsv(img[w][roi_labels==s_idx])
             rgb_intensities = img[w][roi_labels==s_idx]
-            spot_data.extend( np.mean(rgb_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.median(rgb_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.mean(hsv_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.median(hsv_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.std(rgb_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.std(hsv_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.min(rgb_intensities, axis=(1,2)).tolist() )
-            spot_data.extend( np.max(rgb_intensities, axis=(1,2)).tolist() )
+            spot_data[df_cols[2]] = np.mean(rgb_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[3]] = np.mean(rgb_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[4]] = np.mean(rgb_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[5]] = np.median(rgb_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[6]] = np.median(rgb_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[7]] = np.median(rgb_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[8]] = np.mean(hsv_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[9]] = np.mean(hsv_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[10]] = np.mean(hsv_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[11]] = np.median(hsv_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[12]] = np.median(hsv_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[13]] = np.median(hsv_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[14]] = np.std(rgb_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[15]] = np.std(rgb_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[16]] = np.std(rgb_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[17]] = np.std(hsv_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[18]] = np.std(hsv_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[19]] = np.std(hsv_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[20]] = np.min(rgb_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[21]] = np.min(rgb_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[22]] = np.min(rgb_intensities, axis=0).tolist()[2]
+            spot_data[df_cols[23]] = np.max(rgb_intensities, axis=0).tolist()[0]
+            spot_data[df_cols[24]] = np.max(rgb_intensities, axis=0).tolist()[1]
+            spot_data[df_cols[25]] = np.max(rgb_intensities, axis=0).tolist()[2]
 
-            if csvFileName is not None:
-                with open(csvFileName, "w") as f:
-                    #todo write all data to csv
-        #df = pd.DataFrame(data = spot_data, columns = df_cols, index=f"spot_{s_idx:03d}")
-        df_total = pd.concat([df_total, pd.DataFrame(data=spot_data, columns=df_cols, index=f"spot_{s_idx:03d}")], axis=0)
+            # ~ if csvFileName is not None:
+                # ~ with open(csvFileName, "w") as f:
+                    # ~ #todo write all data to csv
+            df_total = pd.concat([df_total, pd.DataFrame(spot_data, index=[pd_idx])], axis=0)
+            pd_idx += 1
     print(df_total)
     return df_total
