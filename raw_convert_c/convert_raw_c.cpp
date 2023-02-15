d70cdc2b33e7f7b367c50b22a19baac5e4325843
zion454 <nas@454.bio>
2023-02-15 16:12:25 -0500

    c code port of raw converter works

--- raw_convert_c/convert_raw_c.cpp
+++ raw_convert_c/convert_raw_c.cpp
@@ -30,45 +30,55 @@ int jpg_to_raw(string in_filepath, string out_filepath)
 		if (strcmp((char*)key_buffer, key_literal) == 0) {
 			cout << "Valid key. Allocation input buffer." << endl;
 			
-			unsigned char * input_buffer = (unsigned char*)malloc(buffer_length);
-			fseek(f, -1 * buffer_length, SEEK_END); //todo include -1?
+			uint8_t * input_buffer = (uint8_t*)malloc(buffer_length);
+			fseek(f, -1 * buffer_length, SEEK_END);
 			fread(input_buffer, 1, buffer_length, f);
 			
 			// Now we have all raw data in input_buffer
 
 			TIFF* tif = TIFFOpen(out_filepath.c_str(), "w"); //opening file here
 
-			TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, IMG_W);
-			TIFFSetField(tif, TIFFTAG_IMAGELENGTH, IMG_H);
-			TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 16);
-			TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 3);
+			TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, (uint32_t)IMG_W);
+			TIFFSetField(tif, TIFFTAG_IMAGELENGTH, (uint32_t)IMG_H);
+			TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, (uint16_t)16);
+			TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, (uint16_t)3);
 			TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
 			TIFFSetField(tif, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
 			TIFFSetField(tif, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
-			
-			unsigned char * dual_line_start;
-			unsigned short * output_line_buffer = (unsigned short*)_TIFFmalloc(IMG_W*2);
-			//~ unsigned short * image = new unsigned short[IMG_W*IMG_H*3];
-			unsigned short currentPixel[3];
-			unsigned short GreenPixel1;
-			unsigned short GreenPixel2; 
+			//todo: change rows per strip? does it increase speed?
+			//TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, 1);
 
-			for (int l=0; l<IMG_H; l++) { //i is line index
+			
+			uint8_t  * dual_line_start;
+			uint16_t * output_line_buffer = (uint16_t*)malloc(6*IMG_W);
+			uint16_t currentPixel[3];
+			uint16_t GreenPixel1;
+			uint16_t GreenPixel2;
+			int res;
+
+			for (int l=0; l<IMG_H; l++) { //l is line index
 				// we want to look at lines 2*l and 2*l+1, which is the following:
 				//memcpy(dual_line_buffer, &input_buffer[2*l*BYTES_PER_LINE], BYTES_PER_LINE*2)
 				//now dual_line_buffer is set up as follows:
-				dual_line_start = &input_buffer[2*l*BYTES_PER_LINE];
+				dual_line_start = &input_buffer[2*l*BYTES_PER_LINE]; //input_buffer + 2*l*BYTES_PER_LINE; //
 				for (int i=0; i < USED_BYTES_PER_LINE; i=i+3) {
-					currentPixel[2] = (unsigned short)( (*(i+dual_line_start) << 4) | (*(i+dual_line_start+2) & 0x0F) ); //blue
-					GreenPixel1     = (unsigned short)( (*(i+dual_line_start+1) << 4) | ((*(i+dual_line_start+2) >> 4) & 0x0F) ); //green1
-					GreenPixel2     = (unsigned short)( (*(i+dual_line_start+BYTES_PER_LINE) << 4) | (*(i+dual_line_start+2+BYTES_PER_LINE) & 0x0F) ); //green2
-					currentPixel[0] = (unsigned short)( (*(i+dual_line_start+1+BYTES_PER_LINE) << 4) | ((*(i+dual_line_start+2+BYTES_PER_LINE) >> 4) & 0x0F) ); //red
-					currentPixel[1] = (GreenPixel1 + GreenPixel2) > 2;
-					memcpy(output_line_buffer+3*i, currentPixel, 6);
+					
+					GreenPixel1     = (uint16_t)( (*(i+dual_line_start) << 4) | (*(i+dual_line_start+2) & 0x0F) ); //green1
+					GreenPixel2     = (uint16_t)( (*(i+dual_line_start+1+BYTES_PER_LINE) << 4) | ((*(i+dual_line_start+2+BYTES_PER_LINE) >> 4) & 0x0F) ); //green2
+					currentPixel[0] = (uint16_t)( (*(i+dual_line_start+1) << 4) | ((*(i+dual_line_start+2) >> 4) & 0x0F) ) << 4; //red
+					currentPixel[2] = (uint16_t)( (*(i+dual_line_start+BYTES_PER_LINE) << 4) | (*(i+dual_line_start+2+BYTES_PER_LINE) & 0x0F) ) << 4; //blue
+					
+					currentPixel[1] = (GreenPixel1 << 3) + (GreenPixel2 << 3);
+					
+					memcpy(output_line_buffer+i, currentPixel, 6);
 				}
-				cout << l << endl;
-				TIFFWriteScanline(tif, output_line_buffer, l, 0);
+				res = TIFFWriteScanline(tif, output_line_buffer, l, 0);
+				if (res < 1){
+					cout << "Error writing scanline" << endl;
+					ret ++;
+				}
 			}
+			cout << "done writing lines, closing tiff file here" << endl;
 			TIFFClose(tif); //closing file here
 			if (output_line_buffer)
 				_TIFFfree(output_line_buffer);
@@ -79,7 +89,6 @@ int jpg_to_raw(string in_filepath, string out_filepath)
 		}
 	}
 	fclose(f);
-	cout << "closing f" << endl;
 	return ret;
 }
 
@@ -93,7 +102,6 @@ int main(int argc, char* argv[])
 	string filepath = argv[1];
 	string target_path = argv[2];
 	int out = jpg_to_raw(filepath, target_path);
-	cout << out << endl;
 	ret = 0;
 	return ret;
 }
