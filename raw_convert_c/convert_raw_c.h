ac89b26e2b521143771841c9a72d5458f831e8ee
zion454 <nas@454.bio>
2023-02-14 20:43:11 -0500

    adding c version of raw converter

---
+++ raw_convert_c/convert_raw_c.h
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+#define OFFSET_FROM_END 18711039 //0x11D81FF
+#define HDR_SIZE 32768
+#define BYTES_PER_LINE 6112
+#define USED_BYTES_PER_LINE 6084
+#define IMG_W 2028
+#define IMG_H 1520
